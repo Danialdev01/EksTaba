7aa2bef69b58210fb325ef180af7fb07d19b07ae
Danial Irfan <developersdani@gmail.com>
2025-03-22 12:50:19 +0800

    edit front page ar

--- index.php
+++ index.php
@@ -19,6 +19,13 @@
                         <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </a> -->
                     <?php $location_index = "."; include('./components/modals/login.php')?>
+
+                    <br>
+                    <a href="./ar/">
+                        <button class="block text-white bg-primary-400 hover:bg-primary-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dankbg-primary-600 dankhover:bg-primary-700 dankfocus:ring-primary-800" type="button">
+                            Play AR Board Game
+                        </button>
+                    </a>
                 </div>
                 <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
                     <img src="./src/assets/images/book-image.png" alt="book image">
