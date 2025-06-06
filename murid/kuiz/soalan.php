61bf023d21a0eb53803f6ea6c3475086394bbe56
Danial Irfan <developersdani@gmail.com>
2025-06-06 15:56:37 +0800

    minor update

--- murid/kuiz/soalan.php
+++ murid/kuiz/soalan.php
@@ -119,7 +119,18 @@ $_SESSION['start'] = false;
                 
                 <div class="pt-20">
 
+                    <div class="max-w-3xl flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50" role="alert">
+                        <svg class="shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                        </svg>
+                        <!-- <span class="sr-only">Info</span> -->
+                        <div>
+                            <span class="font-medium"></span>Berdasarkan ayat dibawah, sila pilih tanda baca yang betul. 
+                        </div>
+                    </div>
                     <div class="danktext-white block max-w-sm p-6 bg-white border border-secondary-200 rounded-lg shadow-sm hover:bg-secondary-100 dankbg-secondary-800 dankborder-secondary-700 dankhover:bg-secondary-700">
+
+                    <!-- add :  -->
                         <h2 class="danktext-white text-xl font-bold mb-4"><?php echo $currentQuestion['teks_soalan']; ?></h2>
                         <form method="POST" id="quiz-form">
                             <?php foreach ($currentQuestion['options'] as $index => $option): ?>
