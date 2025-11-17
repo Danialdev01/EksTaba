8108809d8f66133a2db13952ed84118566f3ee06
Danial Irfan <developersdani@gmail.com>
2025-11-17 03:32:17 +0100

    Update index.php

--- cam/index.php
+++ cam/index.php
@@ -3,7 +3,9 @@
 <head>
     <title>AR Card Game with Multiple Markers</title>
     <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/AR-js-org/AR.js@master/aframe/build/aframe-ar.js"></script>
     <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
+    <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script>
     <style>
         body {
             margin: 0;
