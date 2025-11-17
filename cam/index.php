e2ccc0bce938bee182557147638b9fa1e6a001af
Danial Irfan <developersdani@gmail.com>
2025-11-17 03:41:27 +0100

    Update index.php

--- cam/index.php
+++ cam/index.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <!-- GUNAKAN VERSI INI - paling stabil -->
-    <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
-    <script src="https://raw.githack.com/AR-js-org/AR.js/3.3.0/aframe/build/aframe-ar.js"></script>
+    <!-- <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script> -->
+    <!-- <script src="https://raw.githack.com/AR-js-org/AR.js/3.3.0/aframe/build/aframe-ar.js"></script> -->
+
+    <!-- Cuba kombinasi ini -->
+<script src="https://cdn.jsdelivr.net/gh/aframevr/aframe@1.2.0/dist/aframe-master.min.js"></script>
+<script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
     
     <style>
         body {
