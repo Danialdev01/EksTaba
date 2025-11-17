fc4f4b5ff9e5e7e26603bc9c7d883c9021e50d06
Danial Irfan <developersdani@gmail.com>
2025-11-17 02:23:41 +0100

    Update index.php

--- ar/index.php
+++ ar/index.php
@@ -6,9 +6,8 @@
     <!-- <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script> -->
     <!-- <script src="https://cdn.rawgit.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script> -->
 
-    <!-- Try this combination -->
-    <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
-    <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
+    <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
+    <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-patterns.js"></script>
     <!-- <script src="../ar.js"></script> -->
     <!-- <script src="../aframe.js"></script> -->
     <style>
