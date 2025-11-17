a650330f254a36f714a973546b095a7c19392dd7
Danial Irfan <developersdani@gmail.com>
2025-11-17 03:09:15 +0100

    trial fix

--- cam/index.php
+++ cam/index.php
@@ -3,11 +3,8 @@
 <head>
     <title>AR Card Game with Multiple Markers</title>
     <!-- <meta http-equiv="Content-Security-Policy" content="default-src 'self' https://aframe.io https://cdn.rawgit.com data: blob: 'unsafe-inline' 'unsafe-eval'"> -->
-    <!-- <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script> -->
-    <!-- <script src="https://cdn.rawgit.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script> -->
-
     <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
-    <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-patterns.js"></script>
+    <script src="https://cdn.rawgit.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
     <!-- <script src="../ar.js"></script> -->
     <!-- <script src="../aframe.js"></script> -->
     <style>
--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                     <?php $location_index = "."; include('./components/modals/login.php')?>
 
                     <br>
-                    <a href="./ar/">
+                    <a href="cam/">
                         <button class="block text-white bg-primary-400 hover:bg-primary-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dankbg-primary-600 dankhover:bg-primary-700 dankfocus:ring-primary-800" type="button">
                             Play AR Board Game
                         </button>
